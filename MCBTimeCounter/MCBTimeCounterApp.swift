240d6d9dc83046f6a2ec30f4709a99063ed85942
Marcius Bezerra <marciusbezerra@outlook.com>
2023-06-02 00:13:21 -0300

    Initial Commit

---
+++ MCBTimeCounter/MCBTimeCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MCBTimeCounterApp.swift
+//  MCBTimeCounter
+//
+//  Created by Marcius Bezerra on 02/06/23.
+//
+
+import SwiftUI
+
+@main
+struct MCBTimeCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
